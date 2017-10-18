4f62a9bd26032a6358c8878c7d44b3c45ef5edb2
Jakub Dundalek <dundalek@gmail.com>
2017-10-18 18:22:26 +0200

    Cleanup

--- src/closh/main.cljs
+++ src/closh/main.cljs
@@ -6,32 +6,6 @@
   (:require-macros [alter-cljs.core :refer [alter-var-root]]
                    [closh.reader :refer [patch-reader]]))
 
-; (def spawn (.-spawn (js/require "child_process")))
-
-; (def lumo "/home/me/.nvm/versions/node/v8.4.0/lib/node_modules/lumo-cljs/bin/lumo")
-;
-; (def args
-;   #js["-d" "-e"
-;       "(require '[closh.core :refer
-;                     [read-command expand shx pipe pipe-map pipe-filter get-out-stream]])
-;        (require-macros '[closh.core :refer [sh]])
-;        (def _closh_out_stream_ (.createWriteStream (js/require \"fs\") nil #js{:fd 3}))"
-;       "-r"])
-;
-; (def opts #js{:cwd (.cwd js/process)
-;               :stdio #js["pipe" "pipe" "pipe" (.-stdout js/process)]})
-;
-; (def repl-process (spawn lumo args opts))
-; (.pipe (aget repl-process "stdout") (.-stdout js/process))
-
-; (.write (.-stdin repl-process) "(+ 1 2)\n")
-; (.write (.-stdin repl-process) "(.write _closh_out_stream_ \"test\\n\")\n")
-; (.write (.-stdin repl-process) "(-> (sh ls -l) get-out-stream (.pipe _closh_out_stream_))\n")
-
-; (eval '(defn myfunc [a b] (+ a b)))
-; (eval '(cljs.core$macros/defn myfunc [a b] (+ a b)))
-; (js/console.log "x" (eval '(myfunc 3 4)))
-
 (def readline (js/require "readline"))
 
 (defn -main []
