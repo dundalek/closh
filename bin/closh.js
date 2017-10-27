ed9f9817155a22cffbb311c090c079054174a094
Jakub Dundalek <dundalek@gmail.com>
2017-10-27 12:14:25 +0200

    Load ~/.closhrc on startup

--- bin/closh.js
+++ bin/closh.js
@@ -2,12 +2,21 @@
 'use strict';
 var spawn = require('child_process').spawn;
 var path = require('path');
+var os = require('os');
+var fs = require('fs');
 
 var bin = 'lumo';
 var args = [
   '--classpath', path.join(__dirname, '../src'),
-  '--cache', path.join(process.env.HOME, '.lumo_cache'),
+  '--cache', path.join(os.homedir(), '.lumo_cache'),
   path.join(__dirname, '../src/closh/main.cljs')
 ];
+
+var initFile = path.join(os.homedir(), '.closhrc');
+try {
+  if (fs.statSync(initFile).isFile()) {
+    args = ['--init', initFile].concat(args);
+  }
+} catch (ignore) {}
 
 spawn(bin, args, { stdio: 'inherit' }).on('exit', process.exit);
