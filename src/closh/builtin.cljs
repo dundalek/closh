dc484af5c319ceabe417ded179e183063b6c28f0
Jakub Dundalek <dundalek@gmail.com>
2017-10-28 18:08:30 +0200

    Fix builtin bugs

--- src/closh/builtin.cljs
+++ src/closh/builtin.cljs
@@ -5,12 +5,14 @@
   "Changes current working directory to a path of a first given argument."
   [& args]
   ;; flatten is used because we can get arguments from expand which are collections
-  (process.chdir (first (flatten args))))
+  (js/process.chdir (first (flatten args)))
+  (aset js/process.env "PWD" (js/process.cwd))
+  nil)
 
 (defn exit
   "Exits the process using optional first argument as exit code."
   [& args]
-  (process.exit (first (flatten args))))
+  (js/process.exit (first (flatten args))))
 
 (def quit
   "Alias for `exit`."
