e53be81ce0e8695ee7594145e5cd6fe215546971
Jakub Dundalek <dundalek@gmail.com>
2017-10-31 18:39:07 +0100

    Fix node dependencies in global mode

--- bin/closh.js
+++ bin/closh.js
@@ -7,9 +7,14 @@ var fs = require('fs');
 
 var bin = 'lumo';
 var args = [
-  '--classpath', path.join(__dirname, '../src'),
+  '--classpath', path.join(__dirname, '..', 'src'),
   '--cache', path.join(os.homedir(), '.lumo_cache'),
   '-m', 'closh.main',
 ];
+
+
+// NODE_PATH seems to be missing when running as global binary
+var modules_path = path.join(__dirname, '..', 'node_modules');
+process.env.NODE_PATH = process.env.NODE_PATH ? modules_path + ':' + process.env.NODE_PATH : modules_path;
 
 spawn(bin, args, { stdio: 'inherit' }).on('exit', process.exit);
