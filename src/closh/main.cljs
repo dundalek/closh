02ac076c3e26e2308df8755cc58b7e45418cc699
Jakub Dundalek <dundalek@gmail.com>
2017-11-19 19:03:13 +0100

    Handle history search key events

--- src/closh/main.cljs
+++ src/closh/main.cljs
@@ -24,7 +24,8 @@
 
 (def readline-tty-write readline.Interface.prototype._ttyWrite)
 
-(def readline-state (atom {:history-index -1}))
+(def readline-state (atom {:history-index -1
+                           :mode :input}))
 
 (defn load-init-file
   "Loads init file."
@@ -37,36 +38,35 @@
            (js/console.error "Error while loading " init-path ":\n" e)))))
 
 (defn search-history-prev [{:keys [history-index] :as state} rl]
-  (if (< (inc history-index)
-         (.-history.length rl))
+  (when (< (inc history-index)
+           (.-history.length rl))
     (let [index (inc history-index)
           line (aget rl "history" index)
           cursor (.-length line)]
       (assoc state :history-index index
                    :line line
-                   :cursor cursor))
-    state))
+                   :cursor cursor))))
 
 (defn search-history-next [{:keys [history-index] :as state} rl]
-  (cond
-    (pos? history-index)
+  (when (pos? history-index)
     (let [index (dec history-index)
           line (aget rl "history" index)
           cursor (.-length line)]
       (assoc state :history-index index
                    :line line
-                   :cursor cursor))
+                   :cursor cursor))))
 
-    (zero? history-index)
-    (assoc state :history-index -1
-                 :cursor 0
-                 :line "")
-
-    :default state))
-
-(defn render-line [rl {:keys [line cursor]}]
+(defn render-line [rl {:keys [line cursor prompt mode search-mode query]}]
   (when-not (nil? line) (aset rl "line" line))
   (when-not (nil? cursor) (aset rl "cursor" cursor))
+  (when-let [p (if (and (= mode :search) (not (empty? query)))
+                 (let [label (case search-mode
+                               :prefix "(history-prefix-search)"
+                               :substr "(history-search)"
+                               "(unknown-type-of-search)")]
+                   (str label "`" query "': "))
+                 prompt)]
+    (aset rl "_prompt" p))
   (._refreshLine rl))
 
 (defn prompt
@@ -76,18 +76,87 @@
     (.setPrompt (execute-text "(closh-prompt)"))
     (.prompt true)))
 
-(defn handle-keypress [rl c key]
-  (when-not (or (.-shift key) (.-ctrl key) (.-meta key))
-    (case (.-name key)
-      "up" (do
-             (swap! readline-state search-history-prev rl)
-             (render-line rl @readline-state)
-             true)
-      "down" (do
-               (swap! readline-state search-history-next rl)
-               (render-line rl @readline-state)
-               true)
-      false)))
+(defn key-value [key]
+  ;; escape seems to come with meta always switched on, so lets strip it for now
+  (if (= (.-name key) "escape")
+    "escape"
+    (->>
+      [(when (.-ctrl key) "ctrl")
+       (when (.-meta key) "meta")
+       (when (.-shift key) "shift")
+       (.-name key)]
+      (filter identity)
+      (clojure.string/join "-"))))
+
+(defn restore-previous-state [state]
+  (assoc state
+    :history-index -1
+    :mode :input
+    :prompt (:previous-prompt state)
+    :previous-line nil
+    :previous-prompt nil
+    :query nil))
+
+(defn activate-search-state [state rl search-mode]
+  (assoc state
+    :mode :search
+    :previous-prompt (.-_prompt rl)
+    :previous-line (.-line rl)
+    :query (.-line rl)
+    :search-mode search-mode))
+
+(defn handle-keypress [{:keys [query history-index] :as state} rl c key]
+  ; (assoc state :line (key-value key)))
+; (comment
+  (case (:mode state)
+   :input
+   (case (key-value key)
+     "up" (-> state
+            (activate-search-state rl :prefix)
+            (search-history-prev rl))
+     "ctrl-r" (-> state
+                (activate-search-state rl :substr)
+                (search-history-prev rl))
+     nil)
+
+   :search
+   (case (key-value key)
+     "up" (search-history-prev state rl)
+     "down" (if (zero? history-index)
+              (let [line (or (:previous-line state) "")
+                    cursor (.-length line)]
+                (assoc (restore-previous-state state)
+                       :cursor cursor
+                       :line line))
+              (search-history-next state rl))
+     ;; Accept current line
+     "tab" (restore-previous-state state)
+     ;; Accept and execute current line
+     ;; TODO: Execute
+     "enter" (restore-previous-state state)
+     ;; Cancel search
+     "escape" (let [line (or (:previous-line state) "")
+                    cursor (.-length line)]
+                (assoc (restore-previous-state state)
+                       :cursor cursor
+                       :line line))
+     ;; Cancel search and reset line input
+     "ctrl-c" (assoc (restore-previous-state state)
+                     :line ""
+                     :cursor 0)
+     ;; Search for previous entry (switches to substr search mode if necessary)
+     "ctrl-r" (search-history-prev (assoc state :search-mode :substr) rl)
+     ;; Default case - update search query based on typed character
+     (if-let [q (when (not (or (.-meta key) (.-ctrl key)))
+                  (if (and (not (.-shift key)) (= (.-name key) "backspace"))
+                    (.slice query 0 -1)
+                    (str query c)))]
+       (when (not= query q)
+         (let [next-state (assoc state :query q
+                                       :history-index -1)]
+           (search-history-prev next-state rl)))))
+
+   nil))
 
 (defn -main
   "Starts closh REPL with prompt and readline."
@@ -101,8 +170,11 @@
     (aset rl "_ttyWrite"
       (fn [c key]
         (this-as self
-          (when-not (handle-keypress self c key)
-              (.call readline-tty-write self c key)))))
+          (if-let [state (handle-keypress @readline-state self c key)]
+            (do
+              (reset! readline-state state)
+              (render-line rl state))
+            (.call readline-tty-write self c key)))))
     (init-database
      (fn [err]
        (if err
