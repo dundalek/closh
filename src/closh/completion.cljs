ae3e9ab3a438c386eaad47ed081cc874cedb16e7
Jakub Dundalek <dundalek@gmail.com>
2017-12-14 13:39:38 +0100

    Add doc strings

--- src/closh/completion.cljs
+++ src/closh/completion.cljs
@@ -2,16 +2,20 @@
   (:require [clojure.string]
             [lumo.repl]))
 
-(def child-process (js/require "child_process"))
+(def ^:no-doc child-process (js/require "child_process"))
 
-(defn stream-output [stream cb]
+(defn- stream-output
+  "Helper function to get output from a node stream as a string."
+  [stream cb]
   (let [out #js[]]
     (doto stream
       (.on "data" #(.push out %))
       (.on "end" #(cb nil (.join out "")))
       (.on "error" #(cb % "")))))
 
-(defn get-completions-spawn [cmd args]
+(defn get-completions-spawn
+  "Get completions by spawning a command."
+  [cmd args]
   (js/Promise.
     (fn [resolve reject]
       (let [proc (child-process.spawn cmd args #js{:encoding "utf-8"})]
@@ -22,41 +26,55 @@
                                 (apply array (clojure.string/split (clojure.string/trim stdout) #"\n")))]
               (resolve completions))))))))
 
-(defn complete-fish [line]
+(defn complete-fish
+  "Get completions from a fish shell. Spawns a process."
+  [line]
   (-> (get-completions-spawn (str js/process.env.CLOSH_SOURCES_PATH "/scripts/completion/completion.fish") #js[line])
       (.then (fn [completions] (.map completions #(first (clojure.string/split % #"\t"))))))) ; discard the tab-separated description
 
-(defn complete-bash [line]
+(defn complete-bash
+  "Get completions from bash. Spawns a process."
+  [line]
   (get-completions-spawn (str js/process.env.CLOSH_SOURCES_PATH "/scripts/completion/completion.bash") #js[line]))
 
-(defn complete-zsh [line]
+(defn complete-zsh
+  "Get completions from zsh. Spawns a process."
+  [line]
   (get-completions-spawn (str js/process.env.CLOSH_SOURCES_PATH "/scripts/completion/completion.zsh") #js[line]))
 
-(defn complete-lumo [line]
+(defn complete-lumo
+  "Get completions from Lumo."
+  [line]
   (js/Promise.
     (fn [resolve reject]
       (try
         (lumo.repl/get-completions line resolve)
         (catch :default e (reject e))))))
 
-(defn join-strings-insensitive [line-in s-in]
-  (let [line (clojure.string/lower-case line-in)
-        s (clojure.string/lower-case s-in)]
-    (loop [i (count s)]
+(defn append-completion
+  "Appends completion to a line, discards the common part from in between."
+  [line completion]
+  (let [line-lower (clojure.string/lower-case line)
+        completion-lower (clojure.string/lower-case completion)]
+    (loop [i (count completion-lower)]
       (if (zero? i)
-        (str line-in s-in)
-        (let [sub (subs s 0 i)]
-          (if (clojure.string/ends-with? line sub)
-            (str (subs line-in 0 (- (count line-in) i)) s-in)
+        (str line completion)
+        (let [sub (subs completion-lower 0 i)]
+          (if (clojure.string/ends-with? line-lower sub)
+            (str (subs line 0 (- (count line) i)) completion)
             (recur (dec i))))))))
 
-(defn process-completions [line completions]
+(defn process-completions
+  "Processes completions for a given line, cleans up results by removing duplicates."
+  [line completions]
   (->> completions
-    (map #(join-strings-insensitive line %))
+    (map #(append-completion line %))
     (filter #(not= line %))
     (distinct))) ; bash seems to return duplicates sometimes
 
-(defn complete [line cb]
+(defn complete
+  "Gets completions for a given line. Delegates to existing shells and Lumo. Callback style compatible with node's builtin readline completer function."
+  [line cb]
   (-> (js/Promise.all
        #js[(when (re-find #"\([^)]*$" line) ; only send exprs with unmatched paren to lumo
              (complete-lumo line))
