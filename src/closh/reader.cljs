6565bca08ed48b194192991b11f51fb476e30c73
Jakub Dundalek <dundalek@gmail.com>
2017-12-17 19:38:29 +0100

    Update doc strings

--- src/closh/reader.cljs
+++ src/closh/reader.cljs
@@ -6,32 +6,34 @@
             [cljs.tools.reader.impl.utils :refer [ws-rx]]
             [goog.array :as garray]))
 
-(def read-internal-orig cljs.tools.reader/read*-internal)
+(def ^:no-doc read-internal-orig cljs.tools.reader/read*-internal)
 
-(defn- ^boolean macro-terminating? [ch]
+(defn- ^:no-doc ^boolean macro-terminating?
+  "Customizes `cljs.tools.reader/macro-terminating?` so that read-token is more permissive. For example it does not stop on curly braces but reads them in so they can be used for brace expansion."
+  [ch]
   (case ch
     ; (\" \; \@ \^ \` \~ \( \) \[ \] \{ \} \\) true
     (\" \; \( \) \[ \] \\) true
     false))
 
-(defn ^boolean whitespace?
-  "Checks whether a given character is whitespace"
+(defn- ^:no-doc ^boolean whitespace?
+  "Customizes `cljs.tools.reader.impl.utils/whitespace?` so that read-token splits token only on whitespace and does not split on comma."
   [ch]
   (.test ws-rx ch))
 
-(defn read-token [& args]
-  (with-redefs [cljs.tools.reader/macro-terminating? macro-terminating?
-                cljs.tools.reader.impl.utils/whitespace? whitespace?]
-    (apply cljs.tools.reader/read-token args)))
-
-(defn read-symbol [reader ch]
-  (let [token (read-token reader :symbol ch)
+(defn- ^:no-doc read-token
+  "Reads a non-whitespace token. If it is a valid number it coerces it to number. Otherwise returns it as a symbol."
+  [reader ch]
+  (let [token (with-redefs [cljs.tools.reader/macro-terminating? macro-terminating?
+                            cljs.tools.reader.impl.utils/whitespace? whitespace?]
+                (apply cljs.tools.reader/read-token reader :symbol ch))
         number (js/Number token)]
     (if (js/isNaN number)
       (symbol token)
       number)))
 
-(defn read-internal-custom
+(defn- ^:no-doc read-internal-custom
+  "Customizes `cljs.tools.reader/read*-internal` with our reader enhancements."
   [^not-native reader ^boolean eof-error? sentinel return-on opts pending-forms]
   (loop []
     (log-source reader
@@ -45,7 +47,7 @@
             (nil? ch) (if eof-error? (err/throw-eof-error reader nil) sentinel)
             (identical? ch return-on) READ_FINISHED
             ; (number-literal? reader ch) (read-number reader ch)
-            (= \~ ch) (read-symbol reader ch)
+            (= \~ ch) (read-token reader ch)
             :else (let [f (macros ch)]
                     (if-not (nil? f)
                       (with-redefs [cljs.tools.reader/read*-internal read-internal-orig]
@@ -53,40 +55,43 @@
                           (if (identical? res reader)
                             (recur)
                             res)))
-                      (read-symbol reader ch)))))))))
+                      (read-token reader ch)))))))))
 
-(defn read-orig
+(defn- ^:no-doc read-orig
+  "This is a verbatim copy `cljs.tools.reader/read`. We need a copy otherwise re-binding ends up in infinite loop."
   {:arglists '([] [reader] [opts reader] [reader eof-error? eof-value])}
   ([reader] (read-orig reader true nil))
   ([{eof :eof :as opts :or {eof :eofthrow}} reader] (cljs.tools.reader/read* reader (= eof :eofthrow) eof nil opts (to-array [])))
   ([reader eof-error? sentinel] (cljs.tools.reader/read* reader eof-error? sentinel nil {} (to-array []))))
 
-(defn read [opts reader cb]
-  (with-redefs [cljs.tools.reader/read*-internal read-internal-custom]
-    (loop [coll (transient [])]
-      (let [ch (read-char reader)]
-        (cond
-          (nil? ch) (if-let [result (seq (persistent! coll))]
-                      (cb result)
-                      (read-orig opts reader))
-          (whitespace? ch) (recur coll)
-          :else (do (unread reader ch)
-                    (recur (conj! coll (read-orig opts reader)))))))))
+(defn read
+  "Replacement for a `cljs.tools.reader/read` which allows reading the command mode. It tries to read all input and returns a list of forms. Optionally takes a `transform` function which is called on the valid result."
+  ([reader]
+   (read {} reader))
+  ([opts reader]
+   (read opts reader identity))
+  ([opts reader transform]
+   (with-redefs [cljs.tools.reader/read*-internal read-internal-custom]
+     (loop [coll (transient [])]
+       (let [ch (read-char reader)]
+         (cond
+           (nil? ch) (if-let [result (seq (persistent! coll))]
+                       (transform result)
+                       (read-orig opts reader))
+           (whitespace? ch) (recur coll)
+           :else (do (unread reader ch)
+                     (recur (conj! coll (read-orig opts reader))))))))))
 
 (defn read-sh
+  "Read input in command mode, wrap it in `sh` symbol."
   ([reader]
    (read-sh {} reader))
   ([opts reader]
    (read opts reader #(conj % 'sh))))
 
 (defn read-sh-value
+  "Read input in command mode, wrap it in `sh-value` symbol."
   ([reader]
    (read-sh {} reader))
   ([opts reader]
    (read opts reader #(conj % 'sh-value))))
-
-(defn read-string
-  ([s]
-   (read-string {} s))
-  ([opts s]
-   (read-sh opts (string-push-back-reader s))))
--- test/closh/reader_test.cljs
+++ test/closh/reader_test.cljs
@@ -1,33 +1,34 @@
 (ns closh.reader-test
   (:require [cljs.test :refer-macros [deftest testing is are run-tests]]
-            [closh.reader :refer [read-string]]))
+            [cljs.tools.reader.reader-types :refer [string-push-back-reader]]
+            [closh.reader :refer [read]]))
 
 (deftest test-reader
 
-  (are [x y] (= x (read-string y))
+  (are [x y] (= x (read (string-push-back-reader y)))
 
-    (list 'sh 'ping (symbol "8.8.8.8"))
+    (list 'ping (symbol "8.8.8.8"))
     "ping 8.8.8.8"
 
-    (list 'sh 'ls (symbol "*.{cljc,clj}"))
+    (list 'ls (symbol "*.{cljc,clj}"))
     "ls *.{cljc,clj}"
 
-    (list 'sh 'vim (symbol "~/.closhrc"))
+    (list 'vim (symbol "~/.closhrc"))
     "vim ~/.closhrc"
 
-    (list 'sh 'git 'clone (symbol "git@github.com:dundalek/closh.git"))
+    (list 'git 'clone (symbol "git@github.com:dundalek/closh.git"))
     "git clone git@github.com:dundalek/closh.git"
 
-    '(sh echo $USER/$DISPLAY)
+    '(echo $USER/$DISPLAY)
     "echo $USER/$DISPLAY"
 
-    '(sh echo (+ 2 3))
+    '(echo (+ 2 3))
     "echo (+ 2 3)"
 
-    '(sh echo hi | cat)
+    '(echo hi | cat)
     "echo hi | cat"
 
-    '(sh echo 2 > tmp)
+    '(echo 2 > tmp)
     "echo 2 > tmp")
 
   (is (thrown? js/Error (read-string "echo (str 8.8.8)")))
