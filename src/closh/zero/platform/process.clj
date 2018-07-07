9babb2a7bc4abd836628625001b62ce426bb07ef
Jakub Dundalek <dundalek@gmail.com>
2018-07-07 17:35:20 +0200

    Fix redirection cases

--- src/closh/zero/platform/process.clj
+++ src/closh/zero/platform/process.clj
@@ -28,16 +28,16 @@
 
 (defn builder-redirect
   ([builder fd]
-   (case fd
-     0 (.redirectInput builder)
-     1 (.redirectOutput builder)
-     2 (.redirectError builder)
+   (condp contains? fd
+     #{0 :stdin} (.redirectInput builder)
+     #{1 :stdout} (.redirectOutput builder)
+     #{2 :stderr} (.redirectError builder)
      (throw (Exception. (str "Unsupported file descriptor: " fd " (only file descriptors 0, 1, 2 are supported)")))))
   ([builder fd target]
-   (case fd
-     0 (.redirectInput builder target)
-     1 (.redirectOutput builder target)
-     2 (.redirectError builder target)
+   (condp contains? fd
+     #{0 :stdin} (.redirectInput builder target)
+     #{1 :stdout} (.redirectOutput builder target)
+     #{2 :stderr} (.redirectError builder target)
      (throw (Exception. (str "Unsupported file descriptor: " fd " (only file descriptors 0, 1, 2 are supported)"))))))
 
 ; TODO pass environment variables from *env*
