efb5681657ef34773c5d05c55b559fc76f79d998
Jakub Dundalek <dundalek@gmail.com>
2018-07-21 13:13:21 +0200

    Extract repl-read

--- src/closh/zero/frontend/clojure.clj
+++ src/closh/zero/frontend/clojure.clj
@@ -1,5 +1,23 @@
 (ns closh.zero.frontend.clojure
-  (:require [clojure.main :refer [repl repl-requires]]))
+  (:require [clojure.main :refer [repl repl-requires skip-whitespace skip-if-eol]]))
+
+(defn repl-read
+  "Default :read hook for repl. Reads from *in* which must either be an
+  instance of LineNumberingPushbackReader or duplicate its behavior of both
+  supporting .unread and collapsing all of CR, LF, and CRLF into a single
+  \\newline. repl-read:
+    - skips whitespace, then
+      - returns request-prompt on start of line, or
+      - returns request-exit on end of stream, or
+      - reads an object from the input stream, then
+        - skips the next input character if it's end of line, then
+        - returns the object."
+  [request-prompt request-exit]
+  (or ({:line-start request-prompt :stream-end request-exit}
+       (skip-whitespace *in*))
+      (let [input (read {:read-cond :allow} *in*)]
+        (skip-if-eol *in*)
+        input)))
 
 (defn repl-opt
   "Start a repl with args and inits. Print greeting if no eval options were
@@ -7,7 +25,8 @@
   [[_ & args] inits]
   (println "Clojure REPL" (clojure-version))
   (repl :init (fn []
-                (apply require repl-requires)))
+                (apply require repl-requires))
+        :read repl-read)
   (prn)
   (System/exit 0))
 
