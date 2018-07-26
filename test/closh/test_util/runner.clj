4e297a6d02bacf67676f080fc66609371d70bdcd
Jakub Dundalek <dundalek@gmail.com>
2018-07-26 21:45:37 +0200

    Add ability to hook into test runner reporter

--- test/closh/test_util/runner.clj
+++ test/closh/test_util/runner.clj
@@ -7,12 +7,19 @@
             [closh.common-test]
             [closh.core-test]))
 
+(def report-orig clojure.test/report)
+
+(defn report-custom [& args]
+  ; (clojure.test/with-test-out (println args))
+  (apply report-orig args))
+
 (defn -main[]
-  (time
-    (run-tests
-     'closh.reader-test
-     'closh.compiler-test
-     'closh.common-test
-     'closh.pipeline-test
-     'closh.process-test
-     'closh.core-test)))
+  (binding [clojure.test/report report-custom]
+    (time
+      (run-tests
+       'closh.reader-test
+       'closh.compiler-test
+       'closh.common-test
+       'closh.pipeline-test
+       'closh.process-test
+       'closh.core-test))))
