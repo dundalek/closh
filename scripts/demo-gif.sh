9997b5b12775e1887a552e2980423ebb95ba70b0
Jakub Dundalek <dundalek@gmail.com>
2018-10-05 21:21:06 +0200

    Update demo generator

--- scripts/demo-gif.sh
+++ scripts/demo-gif.sh
@@ -6,9 +6,17 @@
 # sudo apt install randtype
 # go get github.com/sugyan/ttyrec2gif
 
-ttyrecord=ttyrecord
+# Following prompt is used for recording:
+#
+# (def decode-prompt (js/require "decode-prompt"))
+# (def PS1 "\\[\\033[01;32m\\]\\u\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\033[00m\\]\\$ ")
+# (defn closh-prompt []
+#   (decode-prompt PS1 #js{:env js/process.env
+#                          :cwd (path.basename (js/process.cwd))}))
+
+ttyrecord=/tmp/ttyrecord-$$
 gifout=doc/img/demo.gif
 
-ttyrec -e "./scripts/demo-play.sh" $ttyrecord
+env SHELL=/bin/sh ttyrec -e "./scripts/demo-play.sh" $ttyrecord
 ttyrec2gif -in $ttyrecord -out $gifout
 rm $ttyrecord
--- scripts/demo-play.sh
+++ scripts/demo-play.sh
@@ -8,20 +8,27 @@
 # KEY_UP=$'\e'[A
 # echo -n $KEY_UP
 
+# Alternatives:
+# (for [[k v] *1] (str k ": " (count v))) | cat
+# (reduce (fn [m [k v]] (assoc m k (count v))) {} *1)
+# (map (fn [[k v]] [k (count v)]) *1) | (into {})
+# (for [[k v] *1] [k (count v)]) | (into {})
+
 echo "> closh"
-(sleep 2; while read line; do echo "$line" | randtype; sleep 0.3; done << END) | node bin/closh.js
+(sleep 2; while read line; do echo "$line" | randtype; sleep 0.3; done << END) | bin/closh.sh
 date
 (def x "Clojure")
 (str "Hello " x)
 echo (+ 3 4)
 pwd
-cd src/closh
+cd src/closh/zero/platform
 wc -l *
 wc -l * |> (last)
 ls | wc -l
 ls |> (count)
 ls |> (reverse)
-ls |> (group-by #(re-find #"[^.]*$" %))
+(defn extension [s] (re-find #"[^.]*$" s))
+ls |> (group-by extension)
 (for [[k v] *1] (str k ": " (count v))) | cat
 echo The End
 END
