9d922c9f4587782a3ea8af3c2a8f72a04ef8445c
Jakub Dundalek <dundalek@gmail.com>
2019-02-03 23:39:10 +0100

    Tweak custom reader to preserve line info

--- src/closh/zero/frontend/main.clj
+++ src/closh/zero/frontend/main.clj
@@ -1,23 +1,61 @@
 (ns closh.zero.frontend.main
-  (:require [clojure.tools.reader.reader-types :refer [string-push-back-reader push-back-reader]]
+  (:require [clojure.tools.reader.reader-types :refer [string-push-back-reader push-back-reader read-char unread]]
             [closh.zero.reader :refer [read-sh]]
             [closh.zero.platform.process :refer [process?]]
             [closh.zero.env :refer [*closh-environment-requires* *closh-environment-init*]]
-            [closh.zero.utils.clojure-main :refer [repl repl-requires] :as clojure-main])
+            [closh.zero.utils.clojure-main :refer [repl repl-requires with-read-known] :as clojure-main])
   (:refer-clojure :exclude [load-reader])
-  (:import [clojure.lang Compiler RT]
-           [java.io File FileInputStream InputStreamReader StringReader]))
+  (:import [clojure.lang Compiler RT LineNumberingPushbackReader]
+           [java.io File FileInputStream InputStreamReader StringReader PipedWriter PipedReader PushbackReader]))
 
 (def custom-environment
-  (str "(alter-var-root #'load-file (constantly closh.zero.frontend.main/compiler-load-file))"
-       "(def ^{:dynamic true} *args* *command-line-args*)"))
+  (str "(do "
+       "(alter-var-root #'load-file (constantly closh.zero.frontend.main/compiler-load-file))"
+       "(def ^{:dynamic true} *args* *command-line-args*)"
+       " nil)"))
+
+(defn skip-whitespace [reader]
+  (loop []
+    (let [ch (read-char reader)]
+      (if (clojure.tools.reader.impl.utils/whitespace? ch)
+        (recur)
+        (if (nil? ch)
+          false
+          (do (unread reader ch)
+              true))))))
+
+(defn generate-padding [lines columns]
+  (apply str (concat (repeat lines "\n")
+                     (repeat (dec columns) " "))))
 
 (defn make-custom-reader [rdr]
-  (StringReader.
-    (str
-      custom-environment
-      (pr-str *closh-environment-requires*)
-      (closh.zero.reader/read-transform rdr))))
+  (let [eof (Object.)
+        opts {:eof eof :read-cond :allow :features #{:clj}}
+        rdr (LineNumberingPushbackReader. rdr)
+        writer (PipedWriter.)
+        reader (PipedReader. writer)]
+    (doseq [c (str
+                custom-environment
+                (prn-str *closh-environment-requires*))]
+      (.write writer (int c)))
+    (let [custom-reader
+          (proxy [LineNumberingPushbackReader] [reader]
+            (read []
+              (when-not (proxy-super ready)
+                (when (skip-whitespace rdr)
+                  (proxy-super setLineNumber (dec (.getLineNumber rdr)))
+                  (doseq [c (str "\n" (apply str (repeat (dec (.getColumnNumber rdr)) " ")))]
+                    (.write writer (int c))))
+                (let [form (closh.zero.reader/read opts rdr)]
+                  (if (= form eof)
+                    (.close writer)
+                    (do
+                      (doseq [c (pr-str (conj form 'closh.zero.macros/sh-wrapper))]
+                        (.write writer (int c)))))))
+              (let [c (proxy-super read)]
+                c)))]
+      (.setLineNumber custom-reader 0)
+      custom-reader)))
 
 (defn repl-read
   [request-prompt request-exit]
@@ -45,7 +83,7 @@
 (defn compiler-load-file [file]
   (let [f (FileInputStream. file)
         ;; rdr (InputStreamReader. f RT/UTF8)
-        rdr (make-custom-reader (push-back-reader (InputStreamReader. f RT/UTF8)))]
+        rdr (make-custom-reader (PushbackReader. (InputStreamReader. f RT/UTF8)))]
     (try
       (Compiler/load
         rdr
@@ -76,13 +114,17 @@
     ;; (. clojure.lang.Compiler (load rdr)))
     (Compiler/load closh-reader)))
 
-(def eval-opt-orig clojure-main/eval-opt)
-
-(defn eval-opt [s]
-  (eval-opt-orig
-    (str
-      (pr-str *closh-environment-requires*)
-      (closh.zero.reader/read-transform (string-push-back-reader s)))))
+(defn eval-opt
+  "Evals expressions in str, prints each non-nil result using prn"
+  [str]
+  (let [eof (Object.)
+        reader (make-custom-reader (java.io.StringReader. str))]
+      (loop [input (with-read-known (read reader false eof))]
+        (when-not (= input eof)
+          (let [value (eval input)]
+            (when-not (nil? value)
+              (prn value))
+            (recur (with-read-known (read reader false eof))))))))
 
 (defn help-opt
   "Print help text for main"
