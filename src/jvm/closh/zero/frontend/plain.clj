7d787f83d24a2c042e2cb83d42fbc26d2db57b02
Michiel Borkent <michielborkent@gmail.com>
2019-10-23 11:15:00 +0200

    GraalVM compilation works

--- src/jvm/closh/zero/frontend/plain.clj
+++ src/jvm/closh/zero/frontend/plain.clj
@@ -1,9 +1,10 @@
 (ns closh.zero.frontend.plain
   (:gen-class)
-  (:require [closh.zero.platform.eval :as eval]
-            [closh.zero.compiler]
-            [closh.zero.parser]
-            [closh.zero.pipeline]
+  (:require ;; [closh.zero.platform.eval :as eval]
+            ;; [closh.zero.compiler]
+            ;; [closh.zero.parser]
+            ;; [closh.zero.pipeline]
+            [clojure.edn :as edn]
             #_[closh.zero.reader :as reader]
             #_[clojure.tools.reader.reader-types :refer [string-push-back-reader]])
   (:import (java.io PushbackReader StringReader)))
@@ -12,14 +13,15 @@
  (let [eof (Object.)
        opts {:eof eof :read-cond :allow :features #{:clj}}]
    (loop [forms []]
-      (let [form (read opts rdr)]
+      (let [form (edn/read opts rdr)]
         (if (= form eof)
           (seq forms)
           (recur (conj forms form)))))))
 
 (defn -main [& args]
   (let [cmd (or (first args) "echo hello clojure")]
-    (eval/eval
+    (println (read-all (PushbackReader. (StringReader. cmd))))
+    #_(eval/eval
      `(-> ~(closh.zero.compiler/compile-interactive
             (closh.zero.parser/parse (read-all (PushbackReader. (StringReader. cmd)))))
            (closh.zero.pipeline/wait-for-pipeline)))))
