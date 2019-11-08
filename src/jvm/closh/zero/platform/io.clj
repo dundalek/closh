217731b63953cfc49f0ccc633aa19952fa7be4fa
Kirill Salykin <kirill.salykin@gmail.com>
2019-11-08 14:56:57 +0100

    Display dotfile only if explicitly specified

--- src/jvm/closh/zero/platform/io.clj
+++ src/jvm/closh/zero/platform/io.clj
@@ -36,39 +36,45 @@
   ([pattern] (glob pattern nil))
   ([pattern ^String cwd]
    (let [cwd       (java.nio.file.Paths/get cwd (into-array String []))
-         separator (java.io.File/separator)]
-     (->> (str/split pattern (re-pattern separator))
-          (reduce
-           (fn [acc ^String segment]
-             (case segment
-               "."
-               (map #(update % :parts conj ".") acc)
+         separator (java.io.File/separator)
+         result    (->> (str/split pattern (re-pattern separator))
+                        (reduce
+                         (fn [acc ^String segment]
+                           (case segment
+                             "."
+                             (map #(update % :parts conj ".") acc)
 
-               ".."
-               (->> acc
-                    (filter (fn [{:keys [path parts]}]
-                              (directory? path)))
-                    (map (fn [entry]
-                           (-> entry
-                               (update :path #(.getParent ^java.nio.file.Path %))
-                               (update :parts conj ".."))))
-                    (filter (fn [{:keys [path parts]}]
-                              (some? path))))
+                             ".."
+                             (->> acc
+                                  (filter (fn [{:keys [path parts]}]
+                                            (directory? path)))
+                                  (map (fn [entry]
+                                         (-> entry
+                                             (update :path #(.getParent ^java.nio.file.Path %))
+                                             (update :parts conj ".."))))
+                                  (filter (fn [{:keys [path parts]}]
+                                            (some? path))))
 
-               (->> acc
-                    (filter (fn [{:keys [path parts]}]
-                              (directory? path)))
-                    (mapcat (fn [{:keys [^java.nio.file.Path path parts]}]
-                              (map (fn [^java.nio.file.Path nested-path]
-                                     {:path  nested-path
-                                      :parts (conj parts (str (.getFileName nested-path)))})
-                                   (java.nio.file.Files/newDirectoryStream path segment))))
-                    (filter (fn [{:keys [path parts]}]
-                              (some? path))))))
-           [{:path cwd, :parts []}])
-          (map
-           (fn [{:keys [parts]}]
-             (str/join separator parts)))))))
+                             (->> acc
+                                  (filter (fn [{:keys [path parts]}]
+                                            (directory? path)))
+                                  (mapcat (fn [{:keys [^java.nio.file.Path path parts]}]
+                                            (map (fn [^java.nio.file.Path nested-path]
+                                                   {:path  nested-path
+                                                    :parts (conj parts (str (.getFileName nested-path)))})
+                                                 (java.nio.file.Files/newDirectoryStream path segment))))
+                                  (filter (fn [{:keys [^java.nio.file.Path path parts]}]
+                                            (and (some? path)
+                                                 (or (str/starts-with? segment ".")
+                                                     (not (str/starts-with? (.getFileName path) ".")))))))))
+                         [{:path cwd, :parts []}])
+                        (map (fn [{:keys [parts]}]
+                               (str/join separator parts)))
+                        sort)]
+     (if (seq result)
+       result
+       (list pattern)))))
+
 
 (defn out-stream
   "Get stdout stream of a given process."
