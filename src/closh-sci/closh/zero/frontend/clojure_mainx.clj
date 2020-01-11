f0f8fa83fd8b06dc7f2fb768ac70b7cbfeffe773
Jakub Dundalek <dundalek@gmail.com>
2020-01-11 14:20:31 +0100

    wip

--- src/closh-sci/closh/zero/frontend/clojure_mainx.clj
+++ src/closh-sci/closh/zero/frontend/clojure_mainx.clj
@@ -521,7 +521,7 @@ by default when a new command-line REPL is started."} repl-requires
 (defn- initialize
   "Common initialize routine for repl, script, and null opts"
   [args inits]
-  ; (in-ns 'user)
+  (in-ns 'user)
   ; (set! *command-line-args* args)
   (doseq [[opt arg] inits]
     ((init-dispatch opt) arg)))
