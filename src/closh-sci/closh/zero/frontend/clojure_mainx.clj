318c01936ecfeaccb89e2d66a10dc9ec21d2da7e
Jakub Dundalek <dundalek@gmail.com>
2020-01-11 14:35:24 +0100

    WIP

--- src/closh-sci/closh/zero/frontend/clojure_mainx.clj
+++ src/closh-sci/closh/zero/frontend/clojure_mainx.clj
@@ -74,34 +74,34 @@
 ;;;;;;;;;;;;;;;;;;; end of redundantly copied from clojure.repl to avoid dep ;;;;;;;;;;;;;;
 
 
-#_(defmacro with-bindings
-    "Executes body in the context of thread-local bindings for several vars
+(defmacro with-bindings
+  "Executes body in the context of thread-local bindings for several vars
   that often need to be set!: *ns* *warn-on-reflection* *math-context*
   *print-meta* *print-length* *print-level* *compile-path*
   *command-line-args* *1 *2 *3 *e"
-    [& body]
-    `(binding [*ns* *ns*
-               *warn-on-reflection* *warn-on-reflection*
-               *math-context* *math-context*
-               *print-meta* *print-meta*
-               *print-length* *print-length*
-               *print-level* *print-level*
-               *print-namespace-maps* true
-               *data-readers* *data-readers*
-               *default-data-reader-fn* *default-data-reader-fn*
-               *compile-path* (System/getProperty "clojure.compile.path" "classes")
-               *command-line-args* *command-line-args*
-               *unchecked-math* *unchecked-math*
-               *assert* *assert*
-               clojure.spec.alpha/*explain-out* clojure.spec.alpha/*explain-out*
-               *1 nil
-               *2 nil
-               *3 nil
-               *e nil]
-       ~@body))
+  [& body]
+  `(binding [*ns* *ns*
+             *warn-on-reflection* *warn-on-reflection*
+             *math-context* *math-context*
+             *print-meta* *print-meta*
+             *print-length* *print-length*
+             *print-level* *print-level*
+             *print-namespace-maps* true
+             *data-readers* *data-readers*
+             *default-data-reader-fn* *default-data-reader-fn*
+             *compile-path* (System/getProperty "clojure.compile.path" "classes")
+             *command-line-args* *command-line-args*
+             *unchecked-math* *unchecked-math*
+             *assert* *assert*
+             clojure.spec.alpha/*explain-out* clojure.spec.alpha/*explain-out*
+             *1 nil
+             *2 nil
+             *3 nil
+             *e nil]
+     ~@body))
 
-(defmacro with-bindings [& body]
-  `(do ~@body))
+#_(defmacro with-bindings [& body]
+    `(do ~@body))
 
 #_(defn repl-prompt
     "Default :prompt hook for repl"
@@ -522,7 +522,7 @@ by default when a new command-line REPL is started."} repl-requires
   "Common initialize routine for repl, script, and null opts"
   [args inits]
   ; (in-ns 'user)
-  (set! *command-line-args* args)
+  ; (set! *command-line-args* args)
   (doseq [[opt arg] inits]
     ((init-dispatch opt) arg)))
 
