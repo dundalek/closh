d3d6b618211621078df61f5c983f91fdfb32287f
Jakub Dundalek <dundalek@gmail.com>
2020-02-01 15:09:14 +0100

    Test repl interaction with expect

--- scripts/test-sci-binary.sh
+++ scripts/test-sci-binary.sh
@@ -2,12 +2,21 @@
 
 set -x
 
-./closh-zero-sci -e "(+ 1 2)"
-./closh-zero-sci -e "date"
-./closh-zero-sci -e 'echo "hi" | (clojure.string/upper-case)'
-./closh-zero-sci fixtures/script-mode-tests/cond.cljc
-./closh-zero-sci fixtures/script-mode-tests/bar.cljc
-echo "(+ 1 2)" | ./closh-zero-sci -
-echo date | ./closh-zero-sci -
-./closh-zero-sci -h
-echo '(println "Hello REPL!")' | ./closh-zero-sci
+CLOSH_CMD=./closh-zero-sci
+#CLOSH_CMD="java -jar target/closh-zero-sci.jar"
+
+$CLOSH_CMD -e "(+ 1 2)"
+$CLOSH_CMD -e "date"
+$CLOSH_CMD -e 'echo "hi" | (clojure.string/upper-case)'
+$CLOSH_CMD fixtures/script-mode-tests/cond.cljc
+$CLOSH_CMD fixtures/script-mode-tests/bar.cljc
+echo "(+ 1 2)" | $CLOSH_CMD -
+echo date | $CLOSH_CMD -
+$CLOSH_CMD -h
+
+expect -c "
+spawn $CLOSH_CMD;
+send \"(println :hello-word)\r\";
+send \"(exit)\r\";
+expect eof;
+"
