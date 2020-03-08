9493d863f22115e2065ca781d05afff685a2734d
Jakub Dundalek <dundalek@gmail.com>
2020-03-09 00:16:35 +0100

    Simplify sci reader wrapper

--- src/closh-sci/closh/zero/sci_reader.clj
+++ src/closh-sci/closh/zero/sci_reader.clj
@@ -3,20 +3,13 @@
             [clojure.tools.reader.reader-types :as r]))
 
 (defn read-clojure [opts reader]
-  (let [;; edamame fails wtih pushback reader overflow when reader is *in*
+  (let [;; edamame fails with pushback reader overflow when reader is *in*
         ;; TODO figure something better than this workaround
         reader (r/indexing-push-back-reader reader 2)
         ctx (-> (assoc opts :all true)
                 (parser/normalize-opts)
                 (assoc ::parser/expected-delimiter nil))
-        c (r/peek-char reader)
-        loc (parser/location reader)
-        obj (parser/dispatch ctx reader c)
-        ret (if (identical? reader obj)
-              (parser/parse-next ctx reader)
-              (if (instance? clojure.lang.IObj obj)
-                (vary-meta obj merge loc)
-                obj))]
-    (if (identical? ret ::parser/eof)
+        v (parser/parse-next ctx reader)]
+    (if (identical? v ::parser/eof)
       (or (:eof opts) ::parser/eof)
-      ret)))
+      v)))
